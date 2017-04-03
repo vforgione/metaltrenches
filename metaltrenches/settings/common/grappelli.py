dec0e9c7ac6a217e97a4cc52469f16d5d0196027
vforgione <v.forgione@gmail.com>
2017-04-03 15:30:27 -0500

    fresh start
    let's just blow this mother fucker away and start with a clean slate

--- metaltrenches/settings/common/grappelli.py
+++
@@ -1,9 +0,0 @@
-GRAPPELLI_ADMIN_TITLE = 'MetalTrenches Admin'
-
-GRAPPELLI_AUTOCOMPLETE_SEARCH_FIELDS = {
-    'music': {
-        'band': ('id__iexact', 'name__icontains',),
-        'album': ('id__iexact', 'title__icontains', 'band__name__icontains',),
-        'event': ('id__iexact', 'name__icontains',),
-    },
-}
--- metaltrenches/utils/iterators.py
+++
@@ -1,6 +0,0 @@
-from itertools import zip_longest
-
-
-def grouper(iterable, n, fill_value=None):
-    args = [iter(iterable)] * n
-    return zip_longest(*args, fillvalue=fill_value)
